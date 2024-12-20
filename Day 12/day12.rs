972d2b3ea801c8576b22f428c55274183f70e921
Kaz <kaz.middelhoek@ns.nl>
2024-12-20 14:37:47 +0100

    [skip ci] part two

--- Day 12/day12.rs
+++ Day 12/day12.rs
@@ -4,7 +4,7 @@ use std::{
     hash::Hash,
 };
 
-fn part_one_and_two() {
+fn part_one() {
     let garden = fs::read_to_string("Day 12/input.txt")
         .expect("")
         .lines()
@@ -102,6 +102,85 @@ fn look_around(
     return new_region;
 }
 
+fn part_two() {
+    let garden = fs::read_to_string("Day 12/input.txt")
+        .expect("")
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let mut regions_per_plant = HashMap::<char, Vec<HashSet<(usize, usize)>>>::new();
+
+    for (i, line) in garden.iter().enumerate() {
+        for (j, plant) in line.iter().enumerate() {
+            if regions_per_plant.contains_key(plant)
+                && regions_per_plant
+                    .get(plant)
+                    .expect("")
+                    .iter()
+                    .any(|x| x.contains(&(i, j)))
+            {
+                continue;
+            }
+            let mut new_region = HashSet::new();
+            new_region = look_around(&garden, (i, j), new_region);
+
+            regions_per_plant
+                .entry(*plant)
+                .and_modify(|x| x.push(new_region.clone()))
+                .or_insert(vec![new_region.clone()]);
+        }
+    }
+
+    let mut total_price = 0;
+    for (_plant, regions) in regions_per_plant {
+        for region in regions {
+            let mut circum = 0;
+            for pos in &region {
+                // look down
+                if (pos.0 == garden.len() - 1 || !region.contains(&(pos.0 + 1, pos.1)))
+                    && (pos.1 == 0
+                        || !region.contains(&(pos.0, pos.1 - 1))
+                        || (region.contains(&(pos.0, pos.1 - 1))
+                            && region.contains(&(pos.0 + 1, pos.1 - 1))))
+                {
+                    circum += 1
+                }
+                // look up
+                if (pos.0 == 0 || !region.contains(&(pos.0 - 1, pos.1)))
+                    && (pos.1 == 0
+                        || !region.contains(&(pos.0, pos.1 - 1))
+                        || (region.contains(&(pos.0, pos.1 - 1)))
+                            && (pos.0 != 0 && region.contains(&(pos.0 - 1, pos.1 - 1))))
+                {
+                    circum += 1
+                }
+                // look right
+                if (pos.1 == garden[0].len() - 1 || !region.contains(&(pos.0, pos.1 + 1)))
+                    && (pos.0 == 0
+                        || !region.contains(&(pos.0 - 1, pos.1))
+                        || (region.contains(&(pos.0 - 1, pos.1)))
+                            && region.contains(&(pos.0 - 1, pos.1 + 1)))
+                {
+                    circum += 1
+                }
+                // look left
+                if (pos.1 == 0 || !region.contains(&(pos.0, pos.1 - 1)))
+                    && (pos.0 == 0 || // im at top
+                        !region.contains(&(pos.0 - 1, pos.1)) // above is not in my region
+                        || (region.contains(&(pos.0 - 1, pos.1)) // above is in my region, but upper left is too.
+                            && (pos.1!=0 && region.contains(&(pos.0 - 1, pos.1 - 1)))))
+                {
+                    circum += 1
+                }
+            }
+            total_price += region.len() * circum;
+        }
+    }
+    println!("{:?}", total_price)
+}
+
 fn main() {
-    part_one_and_two();
+    part_one();
+    part_two();
 }
